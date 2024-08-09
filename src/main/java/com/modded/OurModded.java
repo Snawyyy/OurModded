1d1472f6f215d9899be5ae5f7d9e258e2ae124dd
Tribi <amitayninio100gmail.com>
2024-08-09 21:14:09 +0300

    added ruby and raw_ruby

--- src/main/java/com/modded/OurModded.java
+++ src/main/java/com/modded/OurModded.java
@@ -1,22 +1,21 @@
 package com.modded;
 
+import com.modded.item.ModItemGroups;
+import com.modded.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OurModded implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("ourmodded");
+
+	public static final String MOD_ID = "ourmodded";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
