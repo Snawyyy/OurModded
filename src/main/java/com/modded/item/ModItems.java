785235ffd127ff06efee4a40024103c27521c3ea
Tribi <amitayninio100gmail.com>
2024-08-09 21:14:58 +0300

    added ruby and raw_ruby

--- src/main/java/com/modded/item/ModItems.java
+++ src/main/java/com/modded/item/ModItems.java
@@ -1,11 +1,9 @@
 package com.modded.item;
 
 import com.modded.OurModded;
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
